b4faca8bf11d949fa78e4558ec8d002abc4b0bfa
Gabriel Guivisdalsky <santiago.kasses@gmail.com>
2023-10-25 10:40:29 -0300

    a

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import * as Contacts from 'expo-contacts';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+import MainScreen from './screens/MainScreen';
 
 export default function App() {
-  useEffect(() => {
-    (async () => {
-      const { status } = await Contacts.requestPermissionsAsync();
-      if (status === 'granted') {
-        const { data } = await Contacts.getContactsAsync({
-          fields: [Contacts.Fields.Emails],
-        });
-
-        if (data.length > 0) {
-          const contact = data[0];
-          console.log(contact);
-        }
-      }
-    })();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Contacts Module Example</Text>
+      <MainScreen/>
     </View>
   );
 }
