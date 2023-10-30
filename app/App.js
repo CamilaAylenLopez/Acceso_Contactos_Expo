63ff35d22e1e4291cf370207dfad5fa03c494e62
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-10-30 19:33:40 -0300

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import * as Contacts from 'expo-contacts';
 
 export default function App() {
-  const [contacts, setContacts] = useEffect()
-    useEffect(() => {
-        (async () => {
-            const { status } = await Contacts.requestPermissionsAsync();
-            if (status === 'granted') {
-                const { data } = await Contacts.getContactsAsync({
-                    fields: [Contacts.Fields.Emails],
-                })
-                if (data.length > 0) {
-                    setContacts(data)
-                }
-            }
-        })();
-    }, []);
+  const [contacts, setContacts] = useState([]);
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        })
+        if (data.length > 0) {
+          setContacts(data)
+        }
+      }
+    })();
+  }, []);
 
-    return (
-        <FlatList
-            data={contacts}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => {
-                return (
-                    <View style={styles.container}>
-                        <Text>{item.name}</Text>
-                        <Text>{item.phoneNumbers && item.phoneNumbers[0] && item.phoneNumbers[0].number}</Text>
-                        <Text>Contacts Module Example</Text>
-                    </View>
-                )
-            }}
-        />
-    );
+  return (
+    <FlatList
+      data={contacts}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => {
+        return (
+          <View style={styles.container}>
+            <Text>{item.name}</Text>
+            <Text>{item.phoneNumbers && item.phoneNumbers[0] && item.phoneNumbers[0].number}</Text>
+            <Text>Contacts Module Example</Text>
+          </View>
+        );
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
